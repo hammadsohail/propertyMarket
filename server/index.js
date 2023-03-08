5e59b455b50fc0f3a1a6897dea9569994539d652
hammadsohail <41212458+hammadsohail@users.noreply.github.com>
2023-03-08 09:21:46 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-
+const port = process.env.PORT || 3000 ;
 app.get('/', (req, res) => {
     res.status(200).json({
       message: 'Welcome to Yariga!',
@@ -22,15 +22,14 @@ app.get('/', (req, res) => {
   app.use('/api/v1/users', userRouter);
   app.use('/api/v1/properties', propertyRouter);
 
-  const startServer = async () => {
-    try {
-      connectDB(process.env.MONGODB_URL);
-    app.listen(process.env.PORT || 3000, () => {
-      console.log(`Server running on port ${process.env.PORT || 3000}`);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+const startServer = async () => {
+  console.log('Starting server...');
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(process.env.PORT || 3000);
+  } catch (error) {
+    console.log(error);
+  }
+};
   startServer();
 
